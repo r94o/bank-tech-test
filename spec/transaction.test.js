669682d27a203795d8fadb44032d72767f5e61c4
Rob Oman <rob.oman@icloud.com>
2022-04-26 18:23:15 +0100

    change to beforeAll to prevent duplicated execution

--- spec/transaction.test.js
+++ spec/transaction.test.js
@@ -2,7 +2,7 @@ const Transaction = require("../lib/transaction")
 
 describe(Transaction, () => {
 
-  beforeEach(() => {
+  beforeAll(() => {
     jest.useFakeTimers()
         .setSystemTime(new Date('2022-03-01'));
     this.transaction = new Transaction(1, 123);
