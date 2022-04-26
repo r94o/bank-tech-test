7c28d12c8a2265b6443d8d9c99060b872b0acd40
Rob Oman <rob.oman@icloud.com>
2022-04-26 12:05:45 +0100

    refactor tests

--- spec/bank_account.test.js
+++ spec/bank_account.test.js
@@ -2,33 +2,33 @@ const BankAccount = require("../lib/bank_account")
 
 describe("Bank Account", () => {
 
+  beforeEach(() => {
+    this.bankAccount = new BankAccount();
+  })
+
   describe("#deposit", () => {
     it("does not raise an error when depositing", () => {
-      const bankAccount = new BankAccount();
       expect(() => {
-        bankAccount.deposit(50)
+        this.bankAccount.deposit(50)
       }).not.toThrow();
     })
   })
 
   describe("#withdraw", () => {
     it("can withdraw money from the account", () => {
-      const bankAccount = new BankAccount();
-      bankAccount.deposit(50);
-      expect(bankAccount.withdraw(50)).toBe(50)
+      this.bankAccount.deposit(50);
+      expect(this.bankAccount.withdraw(50)).toBe(50)
     })
     it("has an opening balance of 0", () => {
-      const bankAccount = new BankAccount();
       expect(() => {
-        bankAccount.withdraw(1)
+        this.bankAccount.withdraw(1)
       }).toThrow("You tried to withdraw more money than your balance");
     })
     it("withdrawing money deducts from the bank balance", () => {
-      const bankAccount = new BankAccount();
-      bankAccount.deposit(50);
-      bankAccount.withdraw(50);
+      this.bankAccount.deposit(50);
+      this.bankAccount.withdraw(50);
       expect(() => {
-        bankAccount.withdraw(1);
+        this.bankAccount.withdraw(1);
       }).toThrow("You tried to withdraw more money than your balance");
     })
   })
